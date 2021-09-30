590a2d4703879c80b5c1582cc12bd4710fc8b083
mattmusc <matteo.muscella@mail.com>
2021-09-30 08:54:17 +0200

    Restores Walt Disney Productions, maps other fields on the Movie type.

--- src/features/movie-detail/index.tsx
+++ src/features/movie-detail/index.tsx
@@ -1,8 +1,12 @@
-import {faArrowAltCircleLeft} from '@fortawesome/free-solid-svg-icons'
+import {faAngleRight, faArrowAltCircleLeft} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {format, parse} from 'date-fns';
 import {useGetMovieQuery} from 'features/movie-detail/api';
 import React from 'react';
 import {Link, useParams} from 'react-router-dom';
+
+const getYear = (x: string | null): string | null =>
+  x && format(parse(x, 'yyyy-mm-dd', new Date()), 'yyyy');
 
 export const MovieDetail = () => {
   const {id} = useParams<{ id: string | undefined }>();
@@ -21,10 +25,45 @@ export const MovieDetail = () => {
               style={{marginRight: '0.5em'}}
             />
           </Link>
-          <h1>{data && data.title}</h1>
+          <h1>
+            {data && data.title}
+            <span className="ms-4 badge bg-secondary">
+              {data && getYear(data.releaseDate)}
+            </span>
+          </h1>
         </div>
 
         <hr/>
+
+        <div className="col-4 mt-4">
+          <div className="card mh-100 shadow-sm rounded">
+            <div className="card-header">
+              <h5>General Infomation</h5>
+            </div>
+            <div className="card-body">
+              {!data && 'Loading...'}
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 mt-4">
+          <div className="card mh-100 shadow-sm rounded">
+            <div className="card-header">
+              <h5>Starring</h5>
+            </div>
+            <div className="card-body">
+              {!data && 'Loading...'}
+              {data && data.starring?.map(actor => (
+                <div key={actor}>
+                  <div className="text-black-50">
+                    <FontAwesomeIcon icon={faAngleRight} className="me-2 text-black"/>
+                    {actor}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
--- src/features/types.ts
+++ src/features/types.ts
@@ -1,9 +1,15 @@
 export interface Movie {
   id: number;
   title: string;
+  country: string | null;
+  language: string | null;
+  runningTime: number | null;
   productionCompany: string[] | null;
   releaseDate: string;
   boxOffice: number | undefined;
   budget: number | undefined;
-  starring: string[] | string | null;
+  starring: string[] | null;
+  directedBy: string[] | null;
+  musicBy: string | null;
+  distributedBy: string | null;
 }
