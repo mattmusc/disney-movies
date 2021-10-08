8ce0325715729bed3d759a747bf52e7960b6865b
mattmusc <matteo.muscella@mail.com>
2021-10-08 11:44:09 +0200

    Adds dotenv package to configure cors and server port.

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,24 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import jsonServer from 'json-server';
 
+dotenv.config();
 const server = express();
 
-const PORT = 5000;
+const PORT = process.env.SERVER_PORT || 5000;
+const CLIENT_URL = process.env.CLIENT_URL;
+const CLIENT_PORT = process.env.CLIENT_PORT;
+const SERVER_URL = process.env.SERVER_URL;
 
-server.use(cors());
+server.use(cors({
+  origin: `${CLIENT_URL}:${CLIENT_PORT}`,
+}));
+
 server.use('/', jsonServer.router('./server/db.json'));
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
+  console.log(`server url: ${SERVER_URL}`)
+  console.log(`client url: ${CLIENT_URL}:${CLIENT_PORT}`)
 });
